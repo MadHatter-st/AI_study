b3aa225073c40afba22acb90f455b5e172dfed91
Rainbow49 <n_khmelev@mail.ru>
2022-11-30 10:34:03 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        /*Hopfield Test = new Hopfield();
+        Test.remember("C:\\\\Users\\\\User\\\\Desktop\\\\tests\\\\A_damaged.png");*/
+
+        GenMaxFun Test = new GenMaxFun();
+        //Perceptron per = new Perceptron();
+        //PerceptronArabian per = new PerceptronArabian();
+        //per.test();
+    }
+}
